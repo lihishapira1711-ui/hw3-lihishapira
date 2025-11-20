1e0cd205b42ad1d3e66aba867f5263db334f0ccd
lihishapi <lihi.shapira@post.runi.ac.il>
2025-11-20 19:27:18 +0200

    Assignment2

--- Anagram.java
+++ Anagram.java
@@ -1,95 +1,64 @@
-
 public class Anagram {
-	public static void main(String args[]) {
-		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+    public static void main(String[] args) {
+        System.out.println(isAnagram("silent","listen"));  // true
+        System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
+        System.out.println(isAnagram("Madam Curie","Radium came")); // true
+        System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
 
-		// Tests the preProcess function.
-		System.out.println(preProcess("What? No way!!!"));
-		
-		// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
-		
-		// Performs a stress test of randomAnagram 
-		String str = "1234567";
-		Boolean pass = true;
-		//// 10 can be changed to much larger values, like 1000
-		for (int i = 0; i < 10; i++) {
-			String randomAnagram = randomAnagram(str);
-			System.out.println(randomAnagram);
-			pass = pass && isAnagram(str, randomAnagram);
-			if (!pass) break;
-		}
-		System.out.println(pass ? "test passed" : "test Failed");
-	}  
+        System.out.println(preProcess("What? No way!!!"));
 
-	// Returns true if the two given strings are anagrams, false otherwise.
-	public static boolean isAnagram(String str1, String str2) {
-	str1 = preProcess(str1);
-    str2 = preProcess(str2);
-	int index;
-	char c;
+        System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
 
-	String cleanStr1 = "";
-    String cleanStr2 = "";
-
-    for (int i = 0; i < str1.length(); i++) {
-        if (str1.charAt(i) != ' ') cleanStr1 += str1.charAt(i);
-    }
-    for (int i = 0; i < str2.length(); i++) {
-        if (str2.charAt(i) != ' ') cleanStr2 += str2.charAt(i);
-    }
-
-	str1= cleanStr1;
-	str2=cleanStr2;
-
-	if (str1.length() != str2.length()) 
-	{
-		 return false;
-	}
-	for(int i=0; i<str1.length(); i++)
-		{
-          c= str1.charAt(i);
-		  index = str2.indexOf(c);
-
-		  if (index==-1)
-		  {
-		 return false;
-
-		  }
-
-		  else 
-		  {
-			str2 = str2.substring(0, index) + str2.substring(index + 1);
-		}
-		}
-	 return true;
-	}
-
-	   
-	
-	public static String preProcess(String str) {
-		String newStr = ""; 
-	   for (int i = 0; i < str.length(); i++)
-        {
-        char c = str.charAt(i);
-		if (Character.isLetter(c)) 
-		{
-             newStr = newStr + Character.toLowerCase(c);
+        String str = "1234567";
+        boolean pass = true;
+        for (int i = 0; i < 10; i++) {
+            String randomAnagram = randomAnagram(str);
+            System.out.println(randomAnagram);
+            pass = pass && isAnagram(str, randomAnagram);
+            if (!pass) break;
         }
-		else if (c == ' ')
-		{
-			newStr = newStr + c;
-		}
-	    }
-		return newStr ;
-	
-	} 
-	   
-	public static String randomAnagram(String str) {
+        System.out.println(pass ? "test passed" : "test Failed");
+    }
+
+    
+    public static String preProcess(String s) {
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                c = (char) (c + ('a' - 'A')); 
+            }
+            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
+                result = result + c; 
+            }
+        }
+        return result;
+    }
+
+    
+    public static boolean isAnagram(String s1, String s2) {
+        
+        String p1 = preProcess(s1);
+        String p2 = preProcess(s2);
+        if (p1.length() != p2.length()) {
+            return false;
+        }
+        for (int i = 0; i < p1.length(); i++) {
+            char charToFind = p1.charAt(i);
+            int indexInP2 = p2.indexOf(charToFind);
+            if (indexInP2 == -1) {
+                return false;
+            } 
+            String partBefore = p2.substring(0, indexInP2);
+            String partAfter = p2.substring(indexInP2 + 1);
+            p2 = partBefore + partAfter;
+        }
+
+        return p2.length() == 0;
+    }
+
+   
+   	public static String randomAnagram(String str) {
 		String temp = str;
 		String newStr="";
 		for(int i=0; i<str.length(); i++)
@@ -102,4 +71,6 @@ public class Anagram {
 		}
 		return newStr;
 	}
+
+  
 }
